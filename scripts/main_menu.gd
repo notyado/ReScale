8da464cbad86eba42b27f512eb956646c705faaf
Seoulrain <bachurin.18@inbox.ru>
2026-07-19 16:55:35 +0300

    logo

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -105,3 +105,6 @@ func _on_play_button_pressed() -> void:
 	outro_tween.chain().tween_callback(func():
 		get_tree().change_scene_to_file("res://scenes/lvl_menu.tscn")
 	)
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
