f4ebb880b46168d670b120c142648e98000e5213
yado <callmeyado@gmail.com>
2026-07-19 18:38:04 +0300

    фикс бага с мостиком

--- scripts/glass_bridge.gd
+++ scripts/glass_bridge.gd
@@ -2,16 +2,21 @@ extends StaticBody2D
 
 @onready var color_rect: ColorRect = $ColorRect
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var detector: Area2D = $Detector
 
-func _on_detector_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		if body.current_data == body.large_data:
-			shatter()
+func _ready() -> void:
+	$Detector/CollisionShape2D.shape = $Detector/CollisionShape2D.shape.duplicate()
+
+func _physics_process(_delta: float) -> void:
+	for body in detector.get_overlapping_bodies():
+		if body.is_in_group("player"):
+			if body.current_data == body.large_data:
+				shatter()
 
 func shatter() -> void:
+	set_physics_process(false) 
 	collision_shape.set_deferred("disabled", true)
 	
 	var tween = create_tween()
 	tween.tween_property(color_rect, "modulate:a", 0.0, 0.15)
-	
 	tween.tween_callback(queue_free)
